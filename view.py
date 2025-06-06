6da86e0da849ec517ad85ecf71aa7919fa3117e3
ke-ta4nonome <k.yoshioka.25.5.1717@4nonome.com>
2025-06-06 14:30:16 +0900

    view昨日の追加

--- view.py
+++ view.py
@@ -3,7 +3,13 @@ import os
 
 CSV_FILE = 'tasks.csv'
 
-# こちらに内容を記入
+df = pd.read_csv(CSV_FILE)
+
+for index, row in df.iterrows():
+    print(f'【{index+1}行目】')
+    for column in df.columns:
+        print(f'{column}: {row[column]}')
+    print('----------------------')
 
 if __name__ == '__main__':
     view_tasks()
